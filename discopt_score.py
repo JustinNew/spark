3a7adbbc4f437b9a4238873b712372a21f2a780a
Justin Tian <tkg8w58@kohls.com>
2018-03-29 10:29:12 -0700

    write functions

--- discopt_score.py
+++ discopt_score.py
@@ -23,8 +23,9 @@ from pyspark.ml.regression import RandomForestRegressor, RandomForestRegressionM
 from pyspark.ml import Pipeline
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.mllib.stat import Statistics
+from discopt_train import castDataFrameColumns
 
-import sys, re
+import sys, re, getopt
 
 # Copy program from local to remote server:
 # scp discopt_score.py gcp-ddh-dev.tst.kohls.com:/home/tkg8w58/projects/DiscountTree
@@ -32,140 +33,140 @@ import sys, re
 # hadoop fs -ls gs://kohls-ddh-lle-spark-root/discount_optimization/discopt_model_15
 
 # Run the program as:
-command_line = "nohup spark-submit --queue long_running --executor-memory 40G --num-executors 6 --jars /opt/hadoop/hadoop-2.8.2/share/hadoop/common/lib/gcs-connector-1.6.0-hadoop2-shaded.jar discopt_score.py 'ce_20180302' 'db_sas_discount' 'ce_20180302_score' 'decisiontree' '15,20,30' 'gs://kohls-ddh-lle-spark-root/discount_optimization/' >& discopt_score.out &"
+command_line = "nohup spark-submit --queue long_running --executor-memory 40G --num-executors 6 --jars /opt/hadoop/hadoop-2.8.2/share/hadoop/common/lib/gcs-connector-1.6.0-hadoop2-shaded.jar discopt_score.py --title=ce_20180302 --database=db_sas_discount --table=ce_20180302_score --model=decisiontree --discounts=15,20,30 --directory=gs://kohls-ddh-lle-spark-root/discount_optimization/ >& discopt_score.out &"
 
-# Command line parameters.
-if len(sys.argv) == 7:
-
-    model_name     = sys.argv[1]
-    database_name  = sys.argv[2]
-    table_score    = sys.argv[3]
-    flag_model     = sys.argv[4]
-    discounts     = sys.argv[5]
-    save_directory = sys.argv[6]
-
-else:
-
-    print("Using the default parameters." + command_line)
-    model_name     = 'ce_20180302'
-    database_name  = 'db_sas_discount'  
-    table_score    = 'ce_20180302_score'
-    flag_model     = 'decisiontree' # Or, 'randomforest'
-    discounts     = '15,20,30'
-    save_directory = 'gs://kohls-ddh-lle-spark-root/discount_optimization/'
-
-# Change the discount levels into integers.
-discounts = discounts.split(",")
-discounts = [int(i) for i in discounts]
-
-#########################################################################
-# Create a spark Session.
-
-spark = SparkSession.builder.appName("Discount Optimization").enableHiveSupport().getOrCreate()
-
-sc = spark.sparkContext
-sc.setLogLevel('FATAL')
-
-sqlContext = SQLContext(sc)
-
-#########################################################################
-#
-# Query to retrieve the customer data incl. account_key, var, val and trgt.
-#
-#########################################################################
-
-print("#########################################################################")
-print("Running Discount Optimization Spark Code To Score.")
-print("The model for sales prediction is " + flag_model + ".")
-print("The output is the sales predictions for all account_keys in SCORE dataset.") 
-print("#########################################################################")
-
-# There are three kinds of records: Discount = 15/20/30. 
-# Get the VARs and associated VALs for each of the Account_Key.
-########################################################################################
-# Get SCORE DATA.
-# Get the VARs and associated VALs for each of the Account_Key.
-sqlStatementInput1 = "select * from " + database_name + "." + table_score + "_vars"
-
-# Retrieve VARs and VALs data.
-bbf = sqlContext.sql(sqlStatementInput1).groupBy("account_key").pivot("var").agg({"val": "sum"})
-
-# Casting columns.
-condition = lambda col: 'F_' in col
-fcols = bbf.select(*filter(condition,bbf.columns)).columns
-for colname in fcols:
-    bbf = bbf.withColumn(colname, bbf[colname].cast("int"))
-
-condition = lambda col: 'R_' in col
-rcols = bbf.select(*filter(condition,bbf.columns)).columns
-for colname in rcols:
-    bbf = bbf.withColumn(colname, bbf[colname].cast("int"))
+if __name__ == '__main__':
     
-condition = lambda col: 'M_' in col
-mcols = bbf.select(*filter(condition,bbf.columns)).columns
-for colname in mcols:
-    bbf = bbf.withColumn(colname, bbf[colname].cast("float"))
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'x', ['title=', 'database=', 'table=', 'discounts=', 'model=','directory='])
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print(err) # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+    
+    # Command line parameters.
+    if len(opts) == 6:
 
-dcols = ["AGE", "ANNIV", "ANNIV_DISC", "FEMALE", "INCOME", "KC_OPEN", "MALE", "MARRIED", "POC", "PREVIOUS_DISCOUNT"]
-for colname in dcols:
-    bbf = bbf.withColumn(colname, bbf[colname].cast("int"))
-########################################################################################
-# Done Get SCORE DATA.   
+        for l, v in opts:
+            if l == '--title':
+                model_name = v
+            elif l == '--database':
+                database_name = v
+            elif l == '--table':
+                table_score = v
+            elif l == '--discounts':
+                discounts = v
+            elif l == '--model':
+                flag_model = v
+            elif l == '--directory':
+                save_directory = v
 
-# Using VectorAssembler to create "features" for pyspark.ml.regression.DecisionTreeRegressor.
-assPath = save_directory + "discopt_model_assembler"
-assembler = VectorAssembler.load(assPath)
-bbf = assembler.transform(bbf)
-    
-# Train the model on all the different discount levels.
-predictions = {}
-nums_p = {}
-for i in discounts: 
-    
-    # Load model and SCORE on records.
-    modelPath = save_directory + "discopt_model_" + model_name + "_" + str(i)
-    
-    # Decide which model to use.
-    if flag_model == 'randomforest':
-        model = RandomForestRegressionModel.load(modelPath)
     else:
-        # Use "minInstancesPerNode=2000" for full samples.
-        model = DecisionTreeRegressionModel.load(modelPath)
+
+        print("Using the default parameters." + command_line)
+        model_name     = 'ce_20180302'
+        database_name  = 'db_sas_discount'  
+        table_score    = 'ce_20180302_score'
+        flag_model     = 'decisiontree' # Or, 'randomforest'
+        discounts     = '15,20,30'
+        save_directory = 'gs://kohls-ddh-lle-spark-root/discount_optimization/'
+
+    # Change the discount levels into integers.
+    discounts = discounts.split(",")
+    discounts = [int(i) for i in discounts]
+
+    #########################################################################
+    # Create a spark Session.
+
+    spark = SparkSession.builder.appName("Discount Optimization").enableHiveSupport().getOrCreate()
+
+    sc = spark.sparkContext
+    sc.setLogLevel('FATAL')
+
+    sqlContext = SQLContext(sc)
+
+    #########################################################################
+    #
+    # Query to retrieve the customer data incl. account_key, var, val and trgt.
+    #
+    #########################################################################
+
+    print("#########################################################################")
+    print("Running Discount Optimization Spark Code To Score.")
+    print("The model for sales prediction is " + flag_model + ".")
+    print("The output is the sales predictions for all account_keys in SCORE dataset.") 
+    print("#########################################################################")
+
+    # There are three kinds of records: Discount = 15/20/30. 
+    # Get the VARs and associated VALs for each of the Account_Key.
+    ########################################################################################
+    # Get SCORE DATA.
+    # Get the VARs and associated VALs for each of the Account_Key.
+    sqlStatementInput1 = "select * from " + database_name + "." + table_score + "_vars"
+
+    # Retrieve VARs and VALs data.
+    bbf = sqlContext.sql(sqlStatementInput1).groupBy("account_key").pivot("var").agg({"val": "sum"})
+
+    # Casting columns.
+    bbf = castDataFrameColumns(bbf)
+
+    ########################################################################################
+    # Done Get SCORE DATA.
+
+    # Using VectorAssembler to create "features" for pyspark.ml.regression.DecisionTreeRegressor.
+    assPath = save_directory + "discopt_model_assembler"
+    assembler = VectorAssembler.load(assPath)
+    bbf = assembler.transform(bbf)
+
+    # Train the model on all the different discount levels.
+    # Add a new column as DISCOUNT label column.
+    predictions = {}
+    nums_p = {}
+    cols = ['account_key', 'prediction', 'discount']
+    for i in discounts:
+
+        # Load model and SCORE on records.
+        modelPath = save_directory + "discopt_model_" + model_name + "_" + str(i)
+
+        # Decide which model to use.
+        if flag_model == 'randomforest':
+            model = RandomForestRegressionModel.load(modelPath)
+        else:
+            # Use "minInstancesPerNode=2000" for full samples.
+            model = DecisionTreeRegressionModel.load(modelPath)
+
+        predictions[i] = model.transform(bbf)
     
-    predictions[i] = model.transform(bbf)
+        predictions[i] = predictions[i].withColumn("discount", lit(i)).select(cols)
+        nums_p[i] = predictions[i].count()
+        print("Number of records in predictions " + str(i) + ' is: ' + str(nums_p[i]))
 
-# Add a new column as DISCOUNT label column.
-cols = ['account_key', 'prediction', 'discount']
-for i in discounts:
-    predictions[i] = predictions[i].withColumn("discount", lit(i)).select(cols)
-    nums_p[i] = predictions[i].count()
-    print("Number of records in predictions " + str(i) + ' is: ' + str(nums_p[i]))
+    # Join all the predictions for different discount tree.
+    if len(discounts) == 1:
+        all_predictions = predictions[discounts[0]]
+    elif len(discounts) == 2:
+        all_predictions = predictions[discounts[0]].union(predictions[discounts[1]])
+    else:
+        all_predictions = predictions[discounts[0]].union(predictions[discounts[1]])
+        for i in range(2, len(discounts)):
+            all_predictions = all_predictions.union(predictions[discounts[i]])
 
-# Join all the predictions for different discount tree.
-if len(discounts) == 1:
-    all_predictions = predictions[discounts[0]]
-elif len(discounts) == 2:
-    all_predictions = predictions[discounts[0]].union(predictions[discounts[1]])
-else:
-    all_predictions = predictions[discounts[0]].union(predictions[discounts[1]])
-    for i in range(2, len(discounts)):
-        all_predictions = all_predictions.union(predictions[discounts[i]])
+    # Select example rows to display.
+    print("Top 100 of Predictions.")
+    all_predictions.select("account_key", "prediction", "discount").show(100)
 
-# Select example rows to display.
-print("Top 100 of Predictions.")
-all_predictions.select("account_key", "prediction", "discount").show(100)
+    print("The average of Spark Predictions for different discount levels.")
+    for i in discounts:
+        print('For discount level ' + str(i) + ':')
+        predictions[i].select(mean("prediction")).show()
 
-print("The average of Spark Predictions for different discount levels.") 
-for i in discounts:
-    print('For discount level ' + str(i) + ':')
-    predictions[i].select(mean("prediction")).show()
-
-# Save the {Spark}_driver into Hive.
-try:
-    name_table = table_score + "_driver_spark"
-    sqlContext.sql("use " + database_name)
-    all_predictions.write.format("orc").saveAsTable(name_table, mode="overwrite")
-except Exception, err:
-    rt.print_exc()
-    raise Exception("writing to Hive table fails")
+    # Save the {Spark}_driver into Hive.
+    try:
+        name_table = table_score + "_driver_spark"
+        sqlContext.sql("use " + database_name)
+        all_predictions.write.format("orc").saveAsTable(name_table, mode="overwrite")
+    except Exception, err:
+        rt.print_exc()
+        raise Exception("writing to Hive table fails")
 
--- discopt_train.py
+++ discopt_train.py
@@ -23,7 +23,7 @@ from pyspark.ml import Pipeline
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.mllib.stat import Statistics
 
-import sys, re
+import sys, re, getopt
 
 # Copy program from local to remote server:
 # scp discopt_train.py gcp-ddh-dev.tst.kohls.com:/home/tkg8w58/projects/DiscountTree
@@ -33,147 +33,192 @@ import sys, re
 # hadoop fs -ls gs://kohls-ddh-lle-spark-root/discount_optimization/discopt_model_15
 
 # Run the program as:
-command_line = "nohup spark-submit --queue long_running --executor-memory 40G --num-executors 6 discopt_train.py 'ce_20180302' 'db_sas_discount' 'data' 'decisiontree' '15,20,30' 'gs://kohls-ddh-lle-spark-root/discount_optimization/' >& discopt_train.out &"
+command_line = "nohup spark-submit --queue long_running --executor-memory 40G --num-executors 6 discopt_train.py --title=ce_20180302 --database=db_sas_discount --table=data --model=decisiontree --discounts=15,20,30 --directory=gs://kohls-ddh-lle-spark-root/discount_optimization/ >& discopt_train.out &"
 
-# Command line parameters.
-if len(sys.argv) == 7:
-
-    model_name     = sys.argv[1]
-    database_name  = sys.argv[2]
-    table_train    = sys.argv[3]
-    flag_model     = sys.argv[4]
-    discounts      = sys.argv[5]
-    save_directory = sys.argv[6]
-
-else:
-
-    print("Using the default parameters." + command_line)
-    model_name     = 'ce_20180302'
-    database_name  = 'db_sas_discount'
-    table_train    = 'data'   
-    flag_model     = 'decisiontree' # Or, 'randomforest'
-    discounts      = '15,20,30'
-    save_directory = 'gs://kohls-ddh-lle-spark-root/discount_optimization/'
-
-# Change the discount levels into integers.
-discounts = discounts.split(",")
-discounts = [int(i) for i in discounts]
-
-#########################################################################
-# Create a spark Session.
-
-spark = SparkSession.builder.appName("Discount Optimization").enableHiveSupport().getOrCreate()
-
-sc = spark.sparkContext
-sc.setLogLevel('FATAL')
-
-sqlContext = SQLContext(sc)
-
-#########################################################################
-#
-# Query to retrieve the customer data incl. sales, demos and target data
-#
-#########################################################################
-
-print("#########################################################################")
-print("Running Discount Optimization Model.")
-print("The model(s) for sales prediction is " + flag_model + ".")
-print("The outputs are models for discounts in " + str(discounts) + " each.") 
-print("#########################################################################")
-
-# There are three kinds of records: Discount = 15/20/30. 
-
-########################################################################################
-# Gets inputs as key value pairs (var & val) for each customer
-sqlStatementInputs1 = "select account_key, var, val from " + database_name + "." + table_train
-
-# transposes th input data data to get one record per customer  
-df1 = sqlContext.sql(sqlStatementInputs1).groupBy("account_key").pivot("var").agg({"val": "sum"})
-
-# Gets the Discount and Target for each customer
-sqlStatementInputs2 = "select distinct account_key, discount, trgt from " + database_name + "." + table_train
-
-# Retrieve Discount and TRGT Data.
-df2 = sqlContext.sql(sqlStatementInputs2)     
-
-# Join inputs with target and discount
-df = df1.join(df2,["account_key"])
-
-# Casting "F_" input columns.
-condition = lambda col: 'F_' in col
-fcols = df.select(*filter(condition,df.columns)).columns
-for colname in fcols:
-    df = df.withColumn(colname, df[colname].cast("int"))
-
-# Casting "R_" input columns.
-condition = lambda col: 'R_' in col
-rcols = df.select(*filter(condition,df.columns)).columns
-for colname in rcols:
-    df = df.withColumn(colname, df[colname].cast("int"))
+###############################################################################################
+# Define a function to casting column types.
+def castDataFrameColumns(ddff):
     
-# Casting "M_" input columns.
-condition = lambda col: 'M_' in col
-mcols = df.select(*filter(condition,df.columns)).columns
-for colname in mcols:
-    df = df.withColumn(colname, df[colname].cast("float"))
+    condition = lambda col: 'F_' in col
+    fcols = ddff.select(*filter(condition,ddff.columns)).columns
+    for colname in fcols:
+        ddff = ddff.withColumn(colname, ddff[colname].cast("int"))
+
+    condition = lambda col: 'R_' in col
+    rcols = ddff.select(*filter(condition,ddff.columns)).columns
+    for colname in rcols:
+        ddff = ddff.withColumn(colname, ddff[colname].cast("int"))
+
+    condition = lambda col: 'M_' in col
+    mcols = ddff.select(*filter(condition,ddff.columns)).columns
+    for colname in mcols:
+        ddff = ddff.withColumn(colname, ddff[colname].cast("float"))
+
+    dcols = ["AGE", "ANNIV", "ANNIV_DISC", "FEMALE", "INCOME", "KC_OPEN", "MALE", "MARRIED", "POC", "PREVIOUS_DISCOUNT"]
+    for colname in dcols:
+        ddff = ddff.withColumn(colname, ddff[colname].cast("int"))
     
-# Casting remaining input columns.
-dcols = ["AGE", "ANNIV", "ANNIV_DISC", "FEMALE", "INCOME", "KC_OPEN", "MALE", "MARRIED", "POC", "PREVIOUS_DISCOUNT"]
-for colname in dcols:
-    df = df.withColumn(colname, df[colname].cast("int"))
+    return ddff
+
+# Define a function to select feature columns.    
+def selectDataFrameColumns(ddff):
     
-# casts the target column
-df = df.withColumn("trgt", df.trgt.cast("float"))
+    condition = lambda col: 'F_' in col
+    fcols = ddff.select(*filter(condition,ddff.columns)).columns
 
-# casts the discount column
-df = df.withColumn("discount", df.discount.cast("int"))
+    condition = lambda col: 'R_' in col
+    rcols = ddff.select(*filter(condition,ddff.columns)).columns
 
-num_obs = df.count()
+    condition = lambda col: 'M_' in col
+    mcols = ddff.select(*filter(condition,ddff.columns)).columns
 
-# Number of observations
-print("Number of observations read: " + str(num_obs))
-
-# Summary about the DataFrame.
-df.describe().show()
-
-# Count the NULL values in each of the columns.
-print('Results about missing value counts.')
-df.select([count(when(isnan(c), c)).alias(c) for c in df.columns]).show()
-
-# Replaces missing values by 0
-df = df.na.fill(0)
-
-########################################################################################
-
-# Using VectorAssembler to create "features"
-assembler = VectorAssembler(inputCols=[x for x in fcols + rcols + mcols + dcols], outputCol='features')
-df = assembler.transform(df)
-assPath = save_directory + "discopt_model_assembler"
-assembler.write().overwrite().save(assPath)
-
-########################################################################################
-
-# Train a tree based model.
-if flag_model == 'randomforest':
-    dt = RandomForestRegressor(featuresCol="features", labelCol="trgt", maxDepth=5, numTrees=50)
-else:
-    # Use "minInstancesPerNode=2000" for full samples.
-    dt = DecisionTreeRegressor(featuresCol="features", labelCol="trgt", maxDepth=8, minInstancesPerNode=2000)
-
-# Train the model on all discount levels.
-for i in discounts: 
+    dcols = ["AGE", "ANNIV", "ANNIV_DISC", "FEMALE", "INCOME", "KC_OPEN", "MALE", "MARRIED", "POC", "PREVIOUS_DISCOUNT"]
+        
+    return fcols + rcols + mcols + dcols
     
-    # Get trainData for all discount levels.
-    trainData = df.filter(df.discount == i)
-    print('For discount level ' + str(i) + ' the trgt average:')
-    trainData.select(mean("trgt")).show()
+###############################################################################################
 
-    # Train model on trainData and Score on testData.
-    model = dt.fit(trainData)
+if __name__ == '__main__':
     
-    # Save the trained model.
-    modelPath = save_directory + "discopt_model_" + model_name + "_" + str(i)
-    model.write().overwrite().save(modelPath)
-    #print(model)
-    print('Written ' + modelPath + ' Successful.')
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'x', ['title=', 'database=', 'table=', 'discounts=', 'model=','directory='])
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print(err) # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+    
+    # Command line parameters.
+    if len(opts) == 6:
 
+        for l, v in opts:
+            if l == '--title':
+                model_name = v
+            elif l == '--database':
+                database_name = v
+            elif l == '--table':
+                table_train = v
+            elif l == '--discounts':
+                discounts = v
+            elif l == '--model':
+                flag_model = v
+            elif l == '--directory':
+                save_directory = v
+
+    else:
+
+        print("Using the default parameters." + command_line)
+        model_name     = 'ce_20180302'
+        database_name  = 'db_sas_discount'
+        table_train    = 'data'
+        flag_model     = 'decisiontree' # Or, 'randomforest'
+        discounts      = '15,20,30'
+        save_directory = 'gs://kohls-ddh-lle-spark-root/discount_optimization/'
+
+    # Change the discount levels into integers.
+    discounts = discounts.split(",")
+    discounts = [int(i) for i in discounts]
+
+    #########################################################################
+    # Create a spark Session.
+
+    spark = SparkSession.builder.appName("Discount Optimization").enableHiveSupport().getOrCreate()
+
+    sc = spark.sparkContext
+    sc.setLogLevel('FATAL')
+
+    sqlContext = SQLContext(sc)
+
+    #########################################################################
+    #
+    # Query to retrieve the customer data incl. sales, demos and target data
+    #
+    #########################################################################
+
+    print("#########################################################################")
+    print("Running Discount Optimization Model.")
+    print("The model(s) for sales prediction is " + flag_model + ".")
+    print("The outputs are models for discounts in " + str(discounts) + " each.")
+    print("#########################################################################")
+
+    # There are three kinds of records: Discount = 15/20/30.
+
+    ########################################################################################
+    # Gets inputs as key value pairs (var & val) for each customer
+    sqlStatementInputs1 = "select account_key, var, val from " + database_name + "." + table_train
+
+    # transposes th input data data to get one record per customer
+    df1 = sqlContext.sql(sqlStatementInputs1).groupBy("account_key").pivot("var").agg({"val": "sum"})
+
+    # Gets the Discount and Target for each customer
+    sqlStatementInputs2 = "select distinct account_key, discount, trgt from " + database_name + "." + table_train
+
+    # Retrieve Discount and TRGT Data.
+    df2 = sqlContext.sql(sqlStatementInputs2)
+
+    # Join inputs with target and discount
+    df = df1.join(df2,["account_key"])
+
+    # Casting columns.
+    df = castDataFrameColumns(df)
+
+    # casts the target column
+    df = df.withColumn("trgt", df.trgt.cast("float"))
+
+    # casts the discount column
+    df = df.withColumn("discount", df.discount.cast("int"))
+
+    num_obs = df.count()
+
+    # Number of observations
+    print("Number of observations read: " + str(num_obs))
+
+    # Count the NULL values in each of the columns.
+    print('Results about missing value counts.')
+    df.select([count(when(isnan(c), c)).alias(c) for c in df.columns]).show()
+
+    # Replaces missing values by 0
+    df = df.na.fill(0)
+
+    ########################################################################################
+
+    # Using VectorAssembler to create "features"
+    featureCols = selectDataFrameColumns(df)
+    assembler = VectorAssembler(inputCols=[x for x in featureCols], outputCol='features')
+    df = assembler.transform(df)
+    assPath = save_directory + "discopt_model_assembler"
+    assembler.write().overwrite().save(assPath)
+
+    ########################################################################################
+
+    # Train a tree based model.
+    if flag_model == 'randomforest':
+        dt = RandomForestRegressor(featuresCol="features", labelCol="trgt", maxDepth=5, numTrees=50)
+    else:
+        # Use "minInstancesPerNode=2000" for full samples.
+        dt = DecisionTreeRegressor(featuresCol="features", labelCol="trgt", maxDepth=8, minInstancesPerNode=2000)
+
+    # Train the model on all discount levels.
+    for i in discounts:
+
+        # Get trainData for all discount levels.
+        trainData = df.filter(df.discount == i)
+        # print('For discount level ' + str(i) + ' the trgt average:')
+        # trainData.select(mean("trgt")).show()
+
+        # Train model on trainData and Score on testData.
+        model = dt.fit(trainData)
+
+        # Save the trained model.
+        modelPath = save_directory + "discopt_model_" + model_name + "_" + str(i)
+        model.write().overwrite().save(modelPath)
+        # print(model)
+        print('Written ' + modelPath + ' Successful.')
+
+        # Make prediction on trainData.
+        # Output RMSE between PREDICTION and TRGT on trainData.
+        predictions = model.transform(trainData)
+
+        evaluator = RegressionEvaluator(labelCol="trgt", predictionCol="prediction", metricName="rmse")
+        rmse = evaluator.evaluate(predictions)
+        print("Root Mean Squared Error (RMSE) on PREDICTION and TRGT on discount %d is %g.\n" % (i, rmse))
